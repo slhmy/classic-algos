e4fd8844bee96e04e92152a3731c2a784a2aa442
slhmy <1484836413@qq.com>
2024-02-25 09:43:32 +0800

    Perf

--- src/union-find-set/union-find-set.cpp
+++ src/union-find-set/union-find-set.cpp
@@ -11,16 +11,16 @@ const int ELEMENT_COUNT = 10;
 int parent[ELEMENT_COUNT];
 
 // to optimize the merge choice
-// use a `rank` array to record info like size/depth of current set
-// you will need to select one in size/depth to help you make decision
-int rank[ELEMENT_COUNT];
+// use a `rank` array to record info like set_size/set_depth of current set
+// you will need to select one in set_size/set_depth to help you make decision
+int set_size[ELEMENT_COUNT];
 
 void init()
 {
     for (int i = 0; i < ELEMENT_COUNT; i++)
     {
         parent[i] = i;
-        rank[i] = 1;
+        set_size[i] = 1;
     }
 }
 
@@ -28,9 +28,13 @@ void init()
 // we need to provide any one of the element in this set
 int find(int e)
 {
+    printf("Finding the root of element %d\n", e);
     // `parent[e] = find(parent[e])` do compress operation while finding the root
     // this will reduce the cost when another query occurs
-    return e == parent[e] ? e : parent[e] = find(parent[e]);
+    int res = e == parent[e] ? e : parent[e] = find(parent[e]);
+    printf("The root of element %d is %d\n", e, res);
+
+    return res;
 }
 
 // `merge` is the function we use when we want to merge two sets together
@@ -42,28 +46,32 @@ void merge(int e1, int e2)
 
     // by comparing the `rank` of each set
     // we choose the merge way & update `rank` if necessary
-    if (rank[p1] < rank[p2])
+    if (set_size[p1] < set_size[p2])
     {
         parent[p1] = p2;
-        // depth not changed
-        rank[p1] += rank[p2];
+        // set_depth not changed
+        set_size[p1] += set_size[p2];
     }
     else
     {
         parent[p2] = p1;
-        // depth[p2] = max(depth[p2], depth[p1]+1);
-        rank[p2] += rank[p1];
+        // mainly for situation `set_depth[p1] == set_depth[p2]`
+        // set_depth[p2] = max(set_depth[p2], set_depth[p1]+1);
+        set_size[p2] += set_size[p1];
     }
+
+    printf("Merged element %d and %d\n", e1, e2);
 }
 
 int main()
 {
+    printf("Init the union find set with %d elements\n", ELEMENT_COUNT);
     init();
-    std::cout << find(1) << std::endl;
+    find(1);
     merge(1, 2);
     merge(3, 4);
-    std::cout << find(2) << std::endl;
-    std::cout << find(3) << std::endl;
+    find(2);
+    find(3);
     merge(2, 3);
-    std::cout << find(4) << std::endl;
+    find(4);
 }
